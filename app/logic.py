ef2b5f959994b9776f49fa3a1e84b832e8a4a7fb
Zeno Sewald <zsewa@outlook.de>
2021-09-04 19:09:44 +0200

    Bump version number

--- app/logic.py
+++ app/logic.py
@@ -816,7 +816,7 @@ class AppLogic:
                     metadata = {
                         "model": {
                             "name": "FederatedPureRegressionSurvivalSVM",
-                            "version": "v0.0.1-alpha",
+                            "version": "v0.1.1-alpha",
                             "privacy_technique": "SMPC" if self.enable_smpc else "None",
                             "training_parameters": {
                                 "alpha": sksurv_obj.alpha,
