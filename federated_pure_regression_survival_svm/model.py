aa7f8085614328b26d6b07a8640bb43901cd801c
Zeno Sewald <zsewa@outlook.de>
2021-09-09 15:26:18 +0200

    Fix unwrapping of aggregated gradient in SMPC

--- federated_pure_regression_survival_svm/model.py
+++ federated_pure_regression_survival_svm/model.py
@@ -418,7 +418,7 @@ class Coordinator(Client):
         aggregated_gradients = result.local_gradient_update
 
         fval = self._calc_fval(aggregated_zeta_sq_sums)
-        gval = aggregated_gradients[0]  # TODO: Why is this even wrapped in a list?
+        gval = aggregated_gradients
         logging.debug(f"fval={fval} and gval={gval}")
         return SteppedEventBasedNewtonCgOptimizer.ResolvedWDependent(
             aggregated_objective_function_value=fval,
