a0eb31110b44f64b4baabdb2da74edc27ead7f84
Zeno Sewald <zsewa@outlook.de>
2021-09-24 15:01:25 +0200

    Add survival_svm stub

---
+++ app/survival_svm/app.py
@@ -0,0 +1,6 @@
+from engine.app import App
+
+# This is the app instance, which holds various values and is used by the app states below
+# You shouldn't access this app instance directly, just ignore it for now
+
+app = App()
